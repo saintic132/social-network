330c12b185b2deb59732b729d03abcdbb9ddc6dc
Ivan Akhremchyk <saintic132@gmail.com>
2020-09-23 17:46:35 +0300

    14-social

---
+++ src/components/Profile.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export function Profile() {
+    return (
+        <div className="app-content">
+            <img
+                src="https://www.gettyimages.pt/gi-resources/images/Homepage/Hero/PT/PT_hero_42_153645159.jpg"
+                alt=""/>
+            <div>
+                <div>
+                    ava + disc
+                </div>
+                my photo
+            </div>
+            <div>
+                my posts
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+        </div>
+    )
+}
