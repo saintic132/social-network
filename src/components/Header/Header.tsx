cefdcb28ae52b03e5cceda7564bf042842e67cc5
saintic <saintic132@gmail.com>
2022-02-25 19:30:04 +0300

    В компоненты добавлены CSS файлы

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import s from "../../App.module.css";
+import s from "./Header.module.css";
 
 function Header() {
     return (
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import s from "../../App.module.css";
+import s from "./Main.module.css";
 
 function Main() {
     return (
