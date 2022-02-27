51945d604cc43c391918e84e04b8e056f2a21b43
saintic <saintic132@gmail.com>
2022-02-27 19:12:11 +0300

    Добавлен className Для активных линков

--- src/components/Main/Nav/Nav.tsx
+++ src/components/Main/Nav/Nav.tsx
@@ -2,23 +2,25 @@ import React from 'react';
 import s from "./Nav.module.css";
 import {NavLink} from "react-router-dom";
 
+const setActive = (navData:any) => navData.isActive ? s.active : ''
+
 function Nav() {
     return (
         <div className={s.nav}>
             <div>
-                <NavLink to='/'>Profile</NavLink>
+                <NavLink to='/' className={setActive}>Profile</NavLink>
             </div>
             <div>
-                <NavLink to='/messages'>Messages</NavLink>
+                <NavLink to='/messages' className={setActive}>Messages</NavLink>
             </div>
             <div>
-                <NavLink to='/news'>News</NavLink>
+                <NavLink to='/news' className={setActive}>News</NavLink>
             </div>
             <div>
-                <NavLink to='/music'>Music</NavLink>
+                <NavLink to='/music' className={setActive}>Music</NavLink>
             </div>
             <div>
-                <NavLink to='/settings'>Settings</NavLink>
+                <NavLink to='/settings' className={setActive}>Settings</NavLink>
             </div>
         </div>
     )
