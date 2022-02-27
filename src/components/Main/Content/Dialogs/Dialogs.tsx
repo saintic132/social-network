3514465b3cff48f2b19063fade5af178f124bde5
saintic <saintic132@gmail.com>
2022-02-27 21:51:08 +0300

    Добавлены предварительные условия для рендера Dialogs

--- src/components/Main/Content/Dialogs/Dialogs.tsx
+++ src/components/Main/Content/Dialogs/Dialogs.tsx
@@ -1,10 +1,39 @@
 import React from "react";
-
+import s from './Dialogs.module.css'
 
 function Dialogs() {
     return (
         <div>
-            Dialogs
+            <div className={s.headerNameDialogs}>
+                <h2>Dialogs</h2>
+            </div>
+            <div className={s.headerForNameAndMessages}>
+                <div className={s.headerName}>
+                    Name:
+                    <div className={s.names}>
+                        <div>
+                            Masha
+                        </div>
+
+                        <div>
+                            Pasha
+                        </div>
+
+                        <div>
+                            Petya
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    Messages:
+                    <div className={s.names}>
+                        <div>
+                            Hello, how are u ?
+                        </div>
+
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
